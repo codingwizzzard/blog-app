85930bfd220061c2d96db02f910589aed09eb25d
harshshah181205 <harshshah123346@gmail.com>
2025-01-03 02:40:58 +0530

    deployment changes

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,9 @@ const PORT = process.env.PORT;
 
 app.use(cors({
     origin: "https://blog-app-chi-amber.vercel.app",
-    credentials: true
+    methods: 'GET,POST,PUT,DELETE',
+    credentials: true,
+    allowedHeaders: 'Content-Type, Authorization',
 }))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
