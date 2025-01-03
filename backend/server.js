6a7f24d195862ea65d2a4f5bc4c84f0f91582b28
Harsh Shah <142344159+codingwizzzard@users.noreply.github.com>
2025-01-03 14:10:49 +0530

    deployment changes

--- backend/server.js
+++ backend/server.js
@@ -8,8 +8,8 @@ const blogRoutes = require("./routes/blog.route.js");
 
 const PORT = process.env.PORT;
 
-app.use('*',cors({
-    origin: "*",
+app.use(cors({
+    origin: "https://blog-app-chi-amber.vercel.app",
     methods: 'GET,POST,PUT,DELETE',
     allowedHeaders: 'Content-Type, Authorization',
 }))
